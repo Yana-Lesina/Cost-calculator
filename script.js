715c987f59042fd22dedd767c53e0c7855b57284
Yana-Lesina <yanalesina3@gmail.com>
2021-10-09 16:32:56 +0300

    H/w of lesson05 + Add_1

--- script.js
+++ script.js
@@ -3,31 +3,60 @@
 //============================================
 //  Переменные 
 //============================================
-let title = prompt('Как называется ваш проект?', ' КаЛьКулятор Верстки');
-let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-let screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?', '1200'));
-let adaptive = prompt('Нужен ли адаптив на сайте?', 'да');
-
-//дополнительные услуги
-let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга1');
-let servicePrice1 = parseFloat(prompt('Сколько это будет стоить?'));
-let service2 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга2');
-let servicePrice2 = parseFloat(prompt('Сколько это будет стоить?'));
-let allServicePrices;
-
-let fullPrice;
-
-let rollback = 10;
-
+let title
+let screens
+let screenPrice
+let adaptive
+let service1
+let service2
+let allServicePrices
+let fullPrice
+let rollback = 10
 let rollbkPercentg;//Процент отката посреднику за работу
 let servicePercentPrice;//Итог. сто-ть за вычетом процента отката
 
 //============================================
 //  Описание функций 
 //============================================
+const isNumber = function (num) {
+  return !isNaN(num) && isFinite(num) && num !== null
+  //ВОПРОС можно ли убрать parseFloat?? в уроке было так: return !isNaN(parseFloat(num)) && isFinite(num) && num !== null 
+ //мне кажетсяя оно стало лишним потому что я добавила parseFloat к prompt
+  
+}
 
-const getAllServicePrices = function(srvPrice1, srvPrice2) {
-  return srvPrice1 + srvPrice2;
+const asking = function() {
+  title = prompt('Как называется ваш проект?', ' КаЛьКулятор Верстки')
+  screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные')
+
+  do {
+    screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?', '1200'))
+  }  while (!isNumber(screenPrice))
+    
+  adaptive = confirm('Нужен ли адаптив на сайте?', 'да')
+}
+
+
+const getAllServicePrices = function() {
+  let sum = 0
+  let checkVar
+  
+
+  for (let i = 0; i < 2; i++) {
+
+    if(i === 0) {
+      service1 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга1')
+    } else if (i === 1) {
+      service2 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга2')
+    }
+
+    do {
+      checkVar = parseFloat(prompt('Сколько это будет стоить?'))
+    } while (!isNumber(checkVar))
+    
+    sum += checkVar
+  }
+  return sum
 };
 
 const getTitle = function(ttle) { 
@@ -72,23 +101,26 @@ const showTypeOf = function(variable) {
 //============================================
 //  Функционал 
 //============================================
-allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+asking();
 
-fullPrice = getFullPrice(screenPrice, allServicePrices);
-
-rollbkPercentg = fullPrice * (rollback/100);
-
-title = getTitle(title);
-
-servicePercentPrice = getServicePercentPrices(fullPrice, rollbkPercentg);
+allServicePrices = getAllServicePrices()
+fullPrice = getFullPrice(screenPrice, allServicePrices)
+rollbkPercentg = fullPrice * (rollback/100)
+servicePercentPrice = getServicePercentPrices(fullPrice, rollbkPercentg)
+title = getTitle(title)
 
 
 //============================================
 //  Консолька 
-//============================================
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
+showTypeOf(title)
+showTypeOf(screens)
+showTypeOf (screenPrice)
+showTypeOf(adaptive)
+showTypeOf(fullPrice)
+showTypeOf(servicePercentPrice)
+
+
+console.log('allServicePrices', allServicePrices)
 
 screens = screens.toLowerCase(); //приведение строки к нижнему регистру
 console.log(screens.split(', ')); //разбиение на массив
