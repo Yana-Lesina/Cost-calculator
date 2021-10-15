90db4d55753ca42fbbd54204ca192d6c168c4903
Yana-Lesina <yanalesina3@gmail.com>
2021-10-15 09:18:40 +0300

    H/w of lesson07

--- script.js
+++ script.js
@@ -1,78 +1,70 @@
 'use strict';
 
-//============================================
-//  Переменные 
-//============================================
-let title
-let screens
-let screenPrice
-let adaptive
-let service1
-let service2
-let allServicePrices
-let fullPrice
-let rollback = 10
-let rollbkPercentg;//Процент отката посреднику за работу
-let servicePercentPrice;//Итог. сто-ть за вычетом процента отката
-
-//============================================
-//  Описание функций 
-//============================================
 const isNumber = function (num) {
-  return !isNaN(num) && isFinite(num) && num !== null
-  //ВОПРОС можно ли убрать parseFloat?? в уроке было так: return !isNaN(parseFloat(num)) && isFinite(num) && num !== null 
- //мне кажетсяя оно стало лишним потому что я добавила parseFloat к prompt
-  
+  return !isNaN(num) && isFinite(num) && num !== null  
 }
 
-const asking = function() {
-  title = prompt('Как называется ваш проект?', ' КаЛьКулятор Верстки')
-  screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные')
+
+const appData = {
+  title: '',
+  screens: '',
+  screenPrice: 0,
+  adaptive: true,
+  rollback: 10,
+  allServicePrices: 0,
+  fullPrice: 0,
+  servicePercentPrice: 0,
+  service1: '',
+  service2: '',
+
+  asking: function() {
+  appData.title = prompt('Как называется ваш проект?', ' КаЛьКулятор Верстки')
+  appData.screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные')
 
   do {
-    screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?', '1200'))
-  }  while (!isNumber(screenPrice))
+    appData.screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?', '20000'))
+  }  while (!isNumber(appData.screenPrice))
     
-  adaptive = confirm('Нужен ли адаптив на сайте?', 'да')
-}
+  appData.adaptive = confirm('Нужен ли адаптив на сайте?', 'да')
+  },
 
-
-const getAllServicePrices = function() {
+  getAllServicePrices: function() {
   let sum = 0
-  let checkVar
+  let price = 0
   
-
   for (let i = 0; i < 2; i++) {
 
     if(i === 0) {
-      service1 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга1')
+      appData.service1 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга1')
     } else if (i === 1) {
-      service2 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга2')
+      appData.service2 = prompt('Какой дополнительный тип услуги нужен?', 'Услуга2')
     }
 
     do {
-      checkVar = parseFloat(prompt('Сколько это будет стоить?'))
-    } while (!isNumber(checkVar))
+      price = parseFloat(prompt('Сколько это будет стоить?'))
+    } while (!isNumber(price))
     
-    sum += checkVar
+    sum += price
   }
   return sum
-};
+  },
 
-const getTitle = function(ttle) { 
-  ttle = ttle.trim().toLowerCase();
-  ttle = ttle[0].toUpperCase() + ttle.slice(1);
- 
-  return ttle;
-};
+  getFullPrice: function() {
+  return appData.screenPrice + appData.allServicePrices;
+  },
 
 
-const getServicePercentPrices = function(vFulPrice, rlbkPerstg) {
-  return Math.ceil(vFulPrice - rlbkPerstg);
-};
+  getServicePercentPrices: function() {
+  return Math.ceil(appData.fullPrice - appData.fullPrice * (appData.rollback/100));
+  },
 
+  getTitle: function() { 
+  appData.title = appData.title.trim().toLowerCase();
+  appData.title = appData.title[0].toUpperCase() + appData.title.slice(1);
+  return appData.title
+  },
 
-const getRollbackMessage = function(price) {
+  getRollbackMessage: function(price) {
   if(price >= 30000){
       return 'Даём скидку в 10%';
 
@@ -85,46 +77,27 @@ const getRollbackMessage = function(price) {
   } else {
     return 'Что то пошло не так:(';
   }
-};
+  },
 
 
-function getFullPrice(scrPrice, allSrvPrice) {
-  return scrPrice + allSrvPrice;
+
+  start: function() {
+    appData.asking();
+
+    appData.allServicePrices = appData.getAllServicePrices()
+    appData.fullPrice = appData.getFullPrice()
+    appData.servicePercentPrice = appData.getServicePercentPrices()
+    appData.title = appData.getTitle()
+
+    appData.logger()
+  }, 
+
+  logger: function() {
+    for(let key in appData) {
+      console.log('Ключ: ' + key + ' Значение: ' + appData[key])
+    }
+  }
 }
 
 
-const showTypeOf = function(variable) {
-  console.log(variable, typeof variable);
-};
-
-
-//============================================
-//  Функционал 
-//============================================
-asking();
-
-allServicePrices = getAllServicePrices()
-fullPrice = getFullPrice(screenPrice, allServicePrices)
-rollbkPercentg = fullPrice * (rollback/100)
-servicePercentPrice = getServicePercentPrices(fullPrice, rollbkPercentg)
-title = getTitle(title)
-
-
-//============================================
-//  Консолька 
-showTypeOf(title)
-showTypeOf(screens)
-showTypeOf (screenPrice)
-showTypeOf(adaptive)
-showTypeOf(fullPrice)
-showTypeOf(servicePercentPrice)
-
-
-console.log('allServicePrices', allServicePrices)
-
-screens = screens.toLowerCase(); //приведение строки к нижнему регистру
-console.log(screens.split(', ')); //разбиение на массив
-
-console.log(getRollbackMessage(fullPrice));
-
-console.log(getServicePercentPrices(fullPrice, rollbkPercentg));
+appData.start()
